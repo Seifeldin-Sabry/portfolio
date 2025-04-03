3dbfc9fc28b5517b8586913882d5f4bc09e236ee
Seifeldin Sabry <93074813+Seifeldin-Sabry@users.noreply.github.com>
2025-04-03 22:33:09 +0200

    fix config lalala

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -57,7 +57,8 @@ export async function POST(request: Request) {
         const transporter = nodemailer.createTransport({
             host: process.env.EMAIL_SERVER_HOST,
             port: Number(process.env.EMAIL_SERVER_PORT),
-            secure: process.env.EMAIL_SERVER_SECURE === "true" && process.env.ENVIRONMENT === "production",
+            secure: false,
+            requireTLS: true,
             auth: {
                 user: process.env.EMAIL_SERVER_USER,
                 pass: process.env.EMAIL_SERVER_PASSWORD,
